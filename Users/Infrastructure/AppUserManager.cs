8b18c883a19f656627232bab66b1bac0f516d348
Kamil <Kamil@VOSTRO>
2016-09-21 22:05:49 +0200

    Commit whole solution

---
+++ Users/Infrastructure/AppUserManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Users.Models;
+namespace Users.Infrastructure
+{
+    public class AppUserManager : UserManager<AppUser>
+    {
+        public AppUserManager(IUserStore<AppUser> store) : base(store)
+        {
+        }
+
+        public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options, IOwinContext context)
+        {
+            AppIdentityDbContext db = context.Get<AppIdentityDbContext>();
+            AppUserManager manager = new AppUserManager(new UserStore<AppUser>(db));
+
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = true,
+                RequireUppercase = true
+            };
+
+            manager.UserValidator = new Microsoft.AspNet.Identity.UserValidator<AppUser>(manager)
+            {
+                RequireUniqueEmail = true
+            };
+
+            return manager;
+        }
+    }
+}
